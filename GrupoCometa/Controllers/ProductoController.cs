5d5261f4aeb7a194c6d3fbf8f36343c7400aeeda
diodax <diosdavi20@gmail.com>
2015-10-17 11:19:19 -0400

    Primer commit

---
+++ GrupoCometa/Controllers/ProductoController.cs
@@ -0,0 +1,87 @@
+﻿using GrupoCometa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrupoCometa.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            List<Producto> listaProductos = new List<Producto>();
+            listaProductos = Producto.GetListaProductos(); 
+            return View(listaProductos);
+        }
+
+        // GET: Producto/Details/
+        public ActionResult Details(int idCodigo)
+        {
+            Producto newModel = new Producto(idCodigo);
+            return View(newModel);
+        }
+
+        // GET: Producto/Insert/
+        public ActionResult Insert()
+        {
+            Producto newModel = new Producto();
+            newModel.listaTiposProducto = Producto.GetListaTiposProducto();
+            return View(newModel);
+        }
+
+        [HttpPost]
+        public ActionResult Insert(Producto newModel)
+        {
+            if(ModelState.IsValid)
+            {
+                newModel.InsertProducto();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                newModel.listaTiposProducto = Producto.GetListaTiposProducto();
+                return View(newModel);
+            }
+        }
+
+        // GET: Producto/Edit/
+        public ActionResult Edit(int idCodigo)
+        {
+            Producto newModel = new Producto(idCodigo);
+            newModel.listaTiposProducto = Producto.GetListaTiposProducto();
+            return View(newModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Producto newModel)
+        {
+            if (ModelState.IsValid)
+            {
+                newModel.UpdateProducto();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                newModel.listaTiposProducto = Producto.GetListaTiposProducto();
+                return View(newModel);
+            }
+        }
+
+        // GET: Producto/Delete/
+        public ActionResult Delete(int idCodigo)
+        {
+            Producto newModel = new Producto(idCodigo);
+            return View(newModel);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int idCodigo, Producto newModel)
+        {
+            Producto.DeleteProducto(idCodigo);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GrupoCometa/Models/Producto.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrupoCometa.Models
+{
+    public class Producto
+    {
+        [Display(Name = "Código")]
+        public int idCodigo { get; set; }
+        [Display(Name = "Nombre")]
+        public string cNombre { get; set; }
+        [Display(Name = "Precio")]
+        public decimal mPrecio { get; set; }
+        [Display(Name = "Cantidad")]
+        public int nCantidad { get; set; }
+        [Display(Name = "Tipo")]
+        public string idTipo { get; set; }
+        [Display(Name = "Modelo")]
+        public string cModelo { get; set; }
+
+        public List<SelectListItem> listaTiposProducto { get; set; }
+
+        /// <summary>
+        /// Constuctor sin parametros 
+        /// </summary>
+        public Producto() { }
+
+        /// <summary>
+        /// Constructor con parametros
+        /// </summary>
+        /// <param name="idCodigo"></param>
+        public Producto(int idCodigo)
+        {
+            Data.dsProductoTableAdapters.ProductosTableAdapter Adapter = new Data.dsProductoTableAdapters.ProductosTableAdapter();
+            Data.dsProducto.ProductosDataTable dt = Adapter.SelectProducto(idCodigo);
+
+            if (dt.Rows.Count > 0)
+            {
+                Data.dsProducto.ProductosRow dr = dt[0];
+                this.idCodigo = dr.idCodigo;
+                this.cNombre = dr.cNombre;
+                this.mPrecio = dr.mPrecio;
+                this.nCantidad = dr.nCantidad;
+                this.idTipo = dr.idTipo;
+                this.cModelo = dr.cModelo;
+            }
+        }
+
+        /// <summary>
+        /// Genera la lista de productos de la DB
+        /// </summary>
+        /// <returns></returns>
+        public static List<Producto> GetListaProductos()
+        {
+            List<Producto> listaProductos = new List<Producto>();
+            Data.dsProductoTableAdapters.ProductosTableAdapter Adapter = new Data.dsProductoTableAdapters.ProductosTableAdapter();
+            Data.dsProducto.ProductosDataTable dt = Adapter.SelectListaProductos();
+
+            foreach(var dr in dt)
+            {
+                Producto item = new Producto();
+                item.idCodigo = dr.idCodigo;
+                item.cNombre = dr.cNombre;
+                item.mPrecio = dr.mPrecio;
+                item.nCantidad = dr.nCantidad;
+                item.idTipo = dr.idTipo;
+                item.cModelo = dr.cModelo;
+                listaProductos.Add(item);
+            }
+
+            return listaProductos;
+        }
+
+        /// <summary>
+        /// Genera el contenido del dropbdown con los tipos de productos
+        /// </summary>
+        /// <returns></returns>
+        public static List<SelectListItem> GetListaTiposProducto()
+        {
+            List<SelectListItem> listaTipos = new List<SelectListItem>();
+            Data.dsProductoTableAdapters.TiposProductoTableAdapter Adapter = new Data.dsProductoTableAdapters.TiposProductoTableAdapter();
+            Data.dsProducto.TiposProductoDataTable dt = Adapter.SelectListaTiposProducto();
+
+            foreach(var dr in dt)
+            {
+                SelectListItem item = new SelectListItem();
+                item.Value = dr.idTipo;
+                item.Text = dr.cDescripcion;
+                listaTipos.Add(item);
+            }
+
+            return listaTipos;
+        }
+
+        /// <summary>
+        /// Inserta el producto a la DB
+        /// </summary>
+        public void InsertProducto()
+        {
+            Data.dsProductoTableAdapters.ProductosTableAdapter Adapter = new Data.dsProductoTableAdapters.ProductosTableAdapter();
+            Adapter.InsertProducto(this.cNombre, this.mPrecio, this.nCantidad, this.idTipo, this.cModelo);
+        }
+
+        /// <summary>
+        /// Actualiza el producto a la DB
+        /// </summary>
+        public void UpdateProducto()
+        {
+            Data.dsProductoTableAdapters.ProductosTableAdapter Adapter = new Data.dsProductoTableAdapters.ProductosTableAdapter();
+            Adapter.UpdateProducto(this.idCodigo, this.cNombre, this.mPrecio, this.nCantidad, this.idTipo, this.cModelo);
+        }
+
+        /// <summary>
+        /// Elimina el producto de la DB
+        /// </summary>
+        /// <param name="idCodigo"></param>
+        public static void DeleteProducto(int idCodigo)
+        {
+            Data.dsProductoTableAdapters.ProductosTableAdapter Adapter = new Data.dsProductoTableAdapters.ProductosTableAdapter();
+            Adapter.DeleteProducto(idCodigo);
+        }
+
+    }
+}
