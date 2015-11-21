b2a71156651f7b1851eace1b067a0a19ea1f9f67
diodax <diosdavi20@gmail.com>
2015-11-21 14:28:31 -0400

    Test branch

---
+++ GrupoCometa/Models/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrupoCometa.Models
+{
+    public class TestClass
+    {
+        public string cTestVar { get; set; }
+    }
+}
