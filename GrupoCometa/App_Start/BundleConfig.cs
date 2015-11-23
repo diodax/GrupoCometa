92835666e8cacb603aee6e8aeddcc50186a6d124
diodax <diosdavi20@gmail.com>
2015-11-23 02:17:14 -0400

    Instalación de Bower Instalación del template sb-admin-2 de startbootstrap Ajustes del nuevo layout Lots of front-end shit

--- GrupoCometa/App_Start/BundleConfig.cs
+++ GrupoCometa/App_Start/BundleConfig.cs
@@ -20,12 +20,34 @@ namespace GrupoCometa
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/bower_components/bootstrap/dist/js/bootstrap.js",  
+                      /*"~/Scripts/bootstrap.js",*/
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                    /*Metis Menu Plugin JavaScript*/
+                      "~/bower_components/metisMenu/dist/metisMenu.js",
+                    /*Morris Charts JavaScript*/
+                      "~/bower_components/raphael/raphael.js",
+                      "~/bower_components/morrisjs/morris.js",
+                    /*Custom Theme JavaScript*/
+                      "~/bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    /*Bootstrap Core CSS*/
+                      "~/bower_components/bootstrap/dist/css/bootstrap.css",
+                    /*MetisMenu CSS*/
+                      "~/bower_components/metisMenu/dist/metisMenu.css",
+                    /*Timeline CSS*/
+                      "~/bower_components/startbootstrap-sb-admin-2/dist/css/timeline.css",
+                    /*Custom CSS*/
+                      "~/bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css",
+                    /*Morris Charts CSS*/
+                      "~/bower_components/morrisjs/morris.css",
+                    /*Custom Fonts*/
+                      "~/bower_components/font-awesome/css/font-awesome.css"
+                      /*"~/Content/bootstrap.css",*/
+                      /*"~/Content/site.css"*/));
         }
     }
 }
