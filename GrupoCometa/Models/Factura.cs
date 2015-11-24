89455b4ef6f022b6fe6b2be9b895447a13929bb9
jcruzz92 <jcruzz92@gmail.com>
2015-11-24 17:18:10 -0400

    Modelo de facturacion (a medias)

---
+++ GrupoCometa/Models/Factura.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrupoCometa.Models
+{
+    public class FacturaHeader
+    {
+        public int idFacturaHeader { get; set; }
+        public int idCliente { get; set; }
+        public int idTipoPago { get; set; }
+        public int idEmpleado { get; set; }
+        public DateTime dtFechaPago { get; set; }
+        public decimal mTotal { get; set; }
+
+        public string cEmpresa { get; set; }
+
+        List<FacturaDetalle> listaDetalle { get; set; }
+
+        public FacturaHeader() { }
+
+        public FacturaHeader(int idFacturaHeader)
+        {
+            Data.dsFacturaTableAdapters.FacturasHeaderTableAdapter Adapter = new Data.dsFacturaTableAdapters.FacturasHeaderTableAdapter();
+            Data.dsFactura.FacturasHeaderDataTable dt = Adapter.SelectListaFacturasHeader(idFacturaHeader);
+
+            if (dt.Rows.Count > 0)
+            {
+                Data.dsFactura.FacturasHeaderRow dr = dt[0];
+                this.idFacturaHeader = dr.idFacturaHeader;
+                this.idCliente = dr.idCliente;
+                this.idTipoPago = dr.idTipoPago;
+                if (!dr.IsidEmpleadoNull())
+                    this.idEmpleado = dr.idEmpleado;
+                if (!dr.IsdtFechaPagoNull())
+                    this.dtFechaPago = dr.dtFechaPago;
+                if (!dr.IsmTotalNull())
+                    this.mTotal = dr.mTotal;
+                if (!dr.IscEmpresaNull())
+                    this.cEmpresa = dr.cEmpresa.Trim();
+
+                //TODO: Populate list
+            }
+        }
+
+
+        public static List<FacturaHeader> GetListaFacturas()
+        {
+            List<FacturaHeader> listaFacturas = new List<FacturaHeader>();
+            Data.dsFacturaTableAdapters.FacturasHeaderTableAdapter Adapter = new Data.dsFacturaTableAdapters.FacturasHeaderTableAdapter();
+            Data.dsFactura.FacturasHeaderDataTable dt = Adapter.SelectListaFacturasHeader(null);
+
+            foreach(var dr in dt)
+            {
+                FacturaHeader temp = new FacturaHeader();
+                temp.idFacturaHeader = dr.idFacturaHeader;
+                temp.idCliente = dr.idCliente;
+                temp.idTipoPago = dr.idTipoPago;
+                if (!dr.IsidEmpleadoNull())
+                    temp.idEmpleado = dr.idEmpleado;
+                if (!dr.IsdtFechaPagoNull())
+                    temp.dtFechaPago = dr.dtFechaPago;
+                if (!dr.IsmTotalNull())
+                    temp.mTotal = dr.mTotal;
+                if (!dr.IscEmpresaNull())
+                    temp.cEmpresa = dr.cEmpresa.Trim();
+
+                listaFacturas.Add(temp);
+            }
+
+            return listaFacturas;
+        }
+
+    }
+
+    public class FacturaDetalle
+    {
+        public int idFacturaDetalle { get; set; }
+        public int idFacturaHeader { get; set; }
+        public int idProducto { get; set; }
+        public int nCantidad { get; set; }
+
+        public FacturaDetalle() { }
+    }
+}
