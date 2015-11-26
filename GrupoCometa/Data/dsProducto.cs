036f39f138b308c04dc240cda3bca17559339266
viksama <vrosario93@gmail.com>
2015-11-26 02:19:06 -0400

    dsProducto.cs se habia eliminado en el merge

---
+++ GrupoCometa/Data/dsProducto.cs
@@ -0,0 +1,18 @@
+﻿namespace GrupoCometa.Data
+{
+}
+
+namespace GrupoCometa.Data
+{
+
+
+    public partial class dsProducto
+    {
+    }
+}
+namespace GrupoCometa.Data {
+    
+    
+    public partial class dsProducto {
+    }
+}
